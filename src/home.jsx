83787ebd458e918095d4872c66173a9f92c6a52e
Pedro Santos <pedrosantosvz@outlook.com>
2023-01-19 19:24:03 -0300

    att

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,9 @@
 import './styles/home.css'
+import './styles/products.css'
 import Walking from './images/walking.jpg'
 import { FaInstagram, FaFacebook } from 'react-icons/fa'
+import Shoes1 from '../src/images/shoes1.png'
+import allShoes from './listProducts.jsx'
 
 
 function Home() {
@@ -18,6 +21,46 @@ function Home() {
                 </div>
             </div>
         </div>
+
+        <div className="containerProducts">
+        <h1 className='titleProduct'></h1>
+        <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+            <div className='product'>
+            <img src={Shoes1} alt='' className='img' />
+            <span className='descriptionProduct'>{allShoes[0].name} - {allShoes[0].price}</span>
+            </div>
+    </div>
 
         
         
--- src/products.jsx
+++ src/products.jsx
@@ -1,53 +1,12 @@
 import './styles/products.css'
 import Shoes1 from '../src/images/shoes1.png'
+import allShoes from './listProducts.jsx'
 
 const products = () => {
 
-    class Product {
-        constructor(color, price) {
-            this.color = color
-            this.price = price
-        }
-    }
-    const tenis1 = new Product ('black', '200');
-    const tenis2 = new Product ('white', '500');
-    const tenis3 = new Product ('blue', '100');
-
-
-    console.log(tenis1)
-
-  
-
   return (
     <>
-    <div className="containerProducts">
-        <h1 className='titleProduct'>Produacts</h1>
-        <div className='product'>
-            <img src={Shoes1} alt='' className='img' />
-            <span className='descriptionProduct'>Confortable Shoes - U$ 100,00</span>
-            </div>
-        <div className='product'>
-            prduto2
-            </div>
-        <div className='product'>
-            prduto3
-            </div>
-        <div className='product'>
-            prduto4
-            </div>
-        <div className='product'>
-            prduto5
-            </div>
-        <div className='product'>
-            prduto6
-            </div>
-        <div className='product'>
-            prduto7
-            </div>
-        <div className='product'>
-            prduto8
-            </div>
-    </div>
+
     </>
 
     
