026749866ebfa51f21a835ffdcca86788f9733f7
Pedro Santos <pedrosantosvz@outlook.com>
2023-01-19 22:33:48 -0300

    att

--- src/listProducts.jsx
+++ src/listProducts.jsx
@@ -10,12 +10,15 @@
     const tenis2 = new Product ('FASTZ Casual','white', '$499.90')
     const tenis3 = new Product ('FASTZ Confortable','blue', '$99.90')
     const tenis4 = new Product ('FASTZ Street','red', '$49.90')
+    const tenis5 = new Product ('FASTZ Hype','yellow', '$1099.90')
 
     const allShoes = [
         tenis1,
         tenis2,
         tenis3,
         tenis4,
+        tenis5
+
     ]
 
 export default allShoes
